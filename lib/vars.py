54111eec5bb0de88c6417bf5925e3e16e1e8fe0b
E. Almqvist <h1g.server@gmail.com>
2020-05-07 15:38:08 +0200

    Tuple = char array :)

--- lib/vars.py
+++ lib/vars.py
@@ -1,7 +1,5 @@
-eng_alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-
-swe_alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "å", "ä", "ö"]
-
+eng_alphabet = *"abcdefghijklmnopqrstuvwxyz",
+swe_alphabet = *"abcdefghijklmnopqrstuvwxyzåäö",
 
 # Definitions
 alphabet = dict()
